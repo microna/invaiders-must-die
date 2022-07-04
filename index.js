3101372a78c2147ef404bc32cf69d16f90ec241e
DESKTOP-0HC73GM\Stas <stas@lucidica.com>
2022-07-04 20:32:33 +0300

    ready to rumble

---
+++ index.js
@@ -0,0 +1,137 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+
+class Player{
+    constructor(){
+      
+
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        this.rotation = 0
+
+        const image = new Image();
+        image.src = 'image/spaceship.png'
+        image.onload = () => {
+            const scale = 0.15
+            this.image= image
+            this.width = image.width * scale
+            this.height = image.height * scale
+            this.position= {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height  - this.height - 20
+            }
+        }
+    }
+    draw(){
+        // c.fillStyle = 'red';
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        c.save()
+        c.translate(player.position.x + player.width / 2, 
+                    player.position.y + player.height / 2)
+        // console.log( player.position.y)
+
+        // c.rotate(this.rotation)
+        // c.translate(
+        //     player.position.x + player.width / 2,
+        //     player.position.y + player.height / 2
+        // )
+      
+       
+            c.drawImage(
+                this.image, 
+                this.position.x, 
+                this.position.y, 
+                this.width, 
+                this.height
+                ) 
+                c.restore()
+    }
+
+    update(){
+        if(this.image){
+            this.draw()
+            this.position.x += this.velocity.x
+        }      
+    }
+}
+
+const player = new Player();
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    },    
+}
+
+function animate(){
+    requestAnimationFrame(animate)
+
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    if(keys.a.pressed && player.position.x >= 0){
+        player.velocity.x = -5
+        player.rotation = -.15
+    } else if (keys.d.pressed && player.position.x +player.width <= canvas.width) {
+        player.velocity.x = 5
+     } else {
+            player.velocity.x = 0
+        }  
+    }
+
+   
+animate()
+
+
+addEventListener('keydown', ({key}) => {
+    switch(key) {
+        case 'ArrowLeft': 
+        console.log('left')
+        
+        keys.a.pressed = true
+        break
+        case 'ArrowRight': 
+        console.log('right')
+        keys.d.pressed = true
+        break
+        case ' ': 
+        console.log('space')
+   
+        break
+       }
+  
+})
+
+addEventListener('keyup', ({key}) => {
+    switch(key) {
+        case 'ArrowLeft': 
+        console.log('left')
+        
+        keys.a.pressed = false
+        break
+        case 'ArrowRight': 
+        console.log('right')
+        keys.d.pressed = false
+        break
+        case ' ': 
+        console.log('space')
+  
+        break
+       }
+  
+})
+
